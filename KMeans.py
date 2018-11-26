b3c48012dca6e50b3b72de295d51158c507054ec
JohanMadsen <johan@bloch-madsen.dk>
2018-11-26 15:24:25 +0100

    generate clustoids?

--- KMeans.py
+++ KMeans.py
@@ -15,7 +15,7 @@ class KMeans(MRJob):
         #Define input file, output file and number of iteration
         super(KMeans, self).configure_args()
         self.add_file_arg('--database')
-        self.add_passthrough_option('--iterations', dest='iterations', default=10, type='int')
+        #self.add_passthrough_option('--iterations', dest='iterations', default=10, type='int')
     def mapper_loader(self, _, line):
         yield None,line
     def reducer_loader(self,key,values):
@@ -30,7 +30,6 @@ class KMeans(MRJob):
     def mapper(self, _, line):
         centroids=line.split("¤")
         centroids=centroids[:-1]
-        #points=self.c.execute("SELECT score1/((SELECT max(score1) from Wiki)-(SELECT min(score1) from Wiki)),score2/((SELECT max(score2) from Wiki)-(SELECT min(score2) from Wiki)),score3/((SELECT max(score3) from Wiki)-(SELECT min(score3) from Wiki)),score4/((SELECT max(score4) from Wiki)-(SELECT min(score4) from Wiki)) from Wiki")
         points=self.c.execute("SELECT (score1-(SELECT min(score1) from Wiki))/((SELECT max(score1) from Wiki)-(SELECT min(score1) from Wiki)),(score2-(SELECT min(score2) from Wiki))/((SELECT max(score2) from Wiki)-(SELECT min(score2) from Wiki)),(score3-(SELECT min(score3) from Wiki))/((SELECT max(score3) from Wiki)-(SELECT min(score3) from Wiki)),(score4-(SELECT min(score4) from Wiki))/((SELECT max(score4) from Wiki)-(SELECT min(score4) from Wiki)) from Wiki")
         for point in points:
             min=-1
@@ -46,21 +45,22 @@ class KMeans(MRJob):
     def reducer(self, Cluster_ID, points):
         points = list(points)
         n = len(points)
-        centroid = [0,0,0,0]
-        for point in points:
-            for i in range(len(point)):
-                centroid[i]+=point[i]
-        centroid = [c/n for c in centroid]
-        s=""
-        for c in centroid:
-            s+=str(c)
-            s+=","
-        s=s[:-1]
-        yield None,s
+        if n>50:
+            centroid = [0,0,0,0]
+            for point in points:
+                for i in range(len(point)):
+                    centroid[i]+=point[i]
+            centroid = [c/n for c in centroid]
+            s=""
+            for c in centroid:
+                s+=str(c)
+                s+=","
+            s=s[:-1]
+            yield None,s
     def steps(self):
         return [MRStep(mapper=self.mapper_loader,reducer=self.reducer_loader),
                        MRStep(mapper_init=self.mapper_init,mapper=self.mapper,reducer=self.reducer)
-                ] * self.options.iterations
+                ] * 100#self.options.iterations
 
 if __name__ == '__main__':
     KMeans.run()
--- setClustersSQL.py
+++ setClustersSQL.py
@@ -9,13 +9,15 @@ def dist(P,C):
 sys.stdin.reconfigure(encoding='utf-8')
 
 conn = sqlite3.connect('WIKI.sqlite')
-c = conn.cursor()
+cursor = conn.cursor()
 centroids=[]
 for line in sys.stdin:
     line=line[:-1]
     split=line.split(",")
     centroids.append(split)
-points=c.execute("SELECT ID,(score1-(SELECT min(score1) from Wiki))/((SELECT max(score1) from Wiki)-(SELECT min(score1) from Wiki)),(score2-(SELECT min(score2) from Wiki))/((SELECT max(score2) from Wiki)-(SELECT min(score2) from Wiki)),(score3-(SELECT min(score3) from Wiki))/((SELECT max(score3) from Wiki)-(SELECT min(score3) from Wiki)),(score4-(SELECT min(score4) from Wiki))/((SELECT max(score4) from Wiki)-(SELECT min(score4) from Wiki)) from Wiki")
+
+cursor.execute("SELECT ID,(score1-(SELECT min(score1) from Wiki))/((SELECT max(score1) from Wiki)-(SELECT min(score1) from Wiki)),(score2-(SELECT min(score2) from Wiki))/((SELECT max(score2) from Wiki)-(SELECT min(score2) from Wiki)),(score3-(SELECT min(score3) from Wiki))/((SELECT max(score3) from Wiki)-(SELECT min(score3) from Wiki)),(score4-(SELECT min(score4) from Wiki))/((SELECT max(score4) from Wiki)-(SELECT min(score4) from Wiki)) from Wiki")
+points=cursor.fetchall()
 for point in points:
     min = -1
     mindist = 1000000000
@@ -25,6 +27,16 @@ for point in points:
         if d < mindist:
             mindist = d
             min = i
-    print(point[0],mindist,min)
-
+    cursor.execute("UPDATE WIKI set cluster_id=? WHERE ID=?",(min,point[0]))
 conn.commit()
+
+count=0
+for cen in centroids:
+    sum = 0
+    for i in range(len(cen)):
+        if i==2:
+            sum+=1-float(cen[i])
+        else:
+            sum+=float(cen[i])
+    print(count,sum)
+    count+=1
